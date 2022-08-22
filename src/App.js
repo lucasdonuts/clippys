ceba34967b690a71f6f93533a4b3e5bd8f798dba
Lucas Duncan <lucasdonuts@gmail.com>
2022-08-22 16:09:28 -0500

    Connected front end to back end

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './stylesheets/App.css';
+import Landing from './components/Landing';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Landing />
     </div>
   );
 }
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,1 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+const Landing = () => {
+  const [ appointments, setAppointments ] = useState([]);
+
+  useEffect( () => {
+    fetch('http://localhost:9292/appointments')
+      .then( res => res.json() )
+      .then( setAppointments )
+  }, [])
+
+  return(
+    <>
+      
+    </>
+    // <ul>
+    //   { appointments.map( appt => {
+    //     return <li key={appt.id}>{appt.time}</li>
+    //   })}
+    // </ul>
+  )
+}
+
+export default Landing;
