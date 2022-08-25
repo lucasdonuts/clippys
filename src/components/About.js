e87664435eaa4c301dbee6260ce7791894ab0f6d
Lucas Duncan <lucasdonuts@gmail.com>
2022-08-25 08:33:45 -0500

    Github and linkedin profile links

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { GrLinkedin } from 'react-icons/gr';
+import { GrGithub } from 'react-icons/gr';
 
 const About = () => {
   return(
@@ -7,13 +8,31 @@ const About = () => {
       <h2 className="about-header mb-5">This webapp created by</h2>
       <ul className="">
         <li className="li-links">
-          Alex Friedman <a href="https://www.linkedin.com/in/alex-friedman-codes/"><GrLinkedin className="li-icon" /></a>
+          Alex Friedman
+          <a href="https://www.linkedin.com/in/alex-friedman-codes/" target="_blank">
+            <GrLinkedin className="li-icon ml-2" />
+          </a>
+          <a href="https://www.github.com/friedmanalexg" target="_blank">
+            <GrGithub className="gh-icon ml-2" />
+          </a>
         </li>
         <li className="li-links">
-          Alex Yang <a href="https://www.linkedin.com/in/xauvkub-yang/"><GrLinkedin className="li-icon" /></a>
+          Alex Yang
+          <a href="https://www.linkedin.com/in/xauvkub-yang/" target="_blank">
+            <GrLinkedin className="li-icon ml-2" />
+          </a>
+          <a href="https://www.github.com/xauvk" target="_blank">
+            <GrGithub className="gh-icon ml-2" />
+          </a>
         </li>
         <li className="li-links">
-          Lucas Duncan <a href="https://www.linkedin.com/in/lucaswdunc/"><GrLinkedin className="li-icon" /></a>
+          Lucas Duncan
+          <a href="https://www.linkedin.com/in/lucaswdunc/" target="_blank">
+            <GrLinkedin className="li-icon ml-2" />
+          </a>
+          <a href="https://www.github.com/lucasdonuts" target="_blank">
+            <GrGithub className="gh-icon ml-2" />
+          </a>
         </li>
       </ul>
       <img className="big-clippy mt-5" src="https://i.imgur.com/btQURSq.png" alt="big-clippy"></img>
